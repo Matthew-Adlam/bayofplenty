02d7b6bb5ffaf5786b7d18cd81a2d5f1dc36c84a
Matthew Adlam <mattadlam3@gmail.com>
2019-03-05 10:58:54 +1300

    Add files via upload

--- blockinspect.js
+++ blockinspect.js
@@ -1,17 +1,9 @@
-var currentHtmlContent;
-var element = new Image();
-var elementWithHiddenContent = document.querySelector("#element-to-hide");
-var innerHtml = elementWithHiddenContent.innerHTML;
+$(document).keydown(function(e){
+    if(e.which === 123){
  
-element.__defineGetter__("id", function() {
+       return false;
  
-    currentHtmlContent= "";
+    }
  
 });
-  
-setInterval(function() { 
-    currentHtmlContent= innerHtml; 
-    console.log(element); 
-    console.clear();  
-    elementWithHiddenContent.innerHTML = currentHtmlContent; 
-}, 1000);
+/* This tells us that pressing the F12 key will do nothing. */
